25ff4898c3dbbe5ef313bbc502544632859cce83
Kazumasa Hirata <50480499+kzmsh@users.noreply.github.com>
2022-01-03 23:50:19 +0900

    エラーメッセージを改良する

--- 9cc.c
+++ 9cc.c
@@ -30,6 +30,21 @@ void error(char *fmt, ...) {
     exit(1);
 }
 
+char *user_input;
+
+void error_at(char *loc, char *fmt, ...) {
+    va_list ap;
+    va_start(ap, fmt);
+
+    int pos = loc - user_input;
+    fprintf(stderr, "%s\n", user_input);
+    fprintf(stderr, "%*s", pos, " ");
+    fprintf(stderr, "^ ");
+    vfprintf(stderr, fmt, ap);
+    fprintf(stderr, "\n");
+    exit(1);
+}
+
 // 次の入力トークンが期待通りの場合、トークンを1つ読み進め、真を返す。
 // それ以外の場合、偽を返す。
 bool consume(char op) {
@@ -44,7 +59,7 @@ bool consume(char op) {
 // それ以外の場合、エラーを報告する。
 void expect(char op) {
     if (token->kind != TK_RESERVED || token->str[0] != op) {
-        error("'%c'ではありません", op);
+        error_at(user_input, "'%c'ではありません", op);
     }
     token = token->next;
 }
@@ -53,7 +68,7 @@ void expect(char op) {
 // それ以外の場合、エラーを報告する。
 int expect_number() {
     if (token->kind != TK_NUM) {
-        error("数値ではありません。");
+        error_at(user_input, "数値ではありません。");
     }
     int val = token->val;
     token = token->next;
@@ -96,7 +111,7 @@ Token *tokenize(char *p) {
             continue;
         }
 
-        error("トークン化できません");
+        error_at(user_input, "トークン化できません");
     }
 
     new_token(TK_EOF, cur, p);
