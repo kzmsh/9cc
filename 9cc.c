81dadda1d5d2b905c3c3402a8476676d192c62a7
Kazumasa Hirata <50480499+kzmsh@users.noreply.github.com>
2022-01-05 09:58:08 +0900

    生成規則を関数で表現する（再帰下降構文解析の実装）

--- 9cc.c
+++ 9cc.c
@@ -131,10 +131,72 @@ typedef struct Node Node;
 
 struct Node {
     NodeKind kind; // ノードの種類
-    Node *lhs;     // 左辺
-    Node *rhs;     // 右辺
+    Node *lhs;     // 左辺 left-hand side
+    Node *rhs;     // 右辺 right-hand side
     int val;       // kindがND_NUMの場合の値
 };
+
+Node *new_node(NodeKind kind, Node *lhs, Node *rhs) {
+    Node *node = calloc(1, sizeof(Node));
+    node->kind = kind;
+    node->lhs = lhs;
+    node->rhs = rhs;
+    return node;
+}
+
+Node *new_node_num(int val) {
+    Node *node = calloc(1, sizeof(Node));
+    node->kind = ND_NUM;
+    node->val = val;
+    return node;
+}
+
+// expr = mul ("+" mul | "-" mul)*
+Node *expr();
+
+// mul = primary ("*" primary | "/" primary)*
+Node *mul();
+
+// primary = num | "(" expr ")"
+Node *primary();
+
+Node *expr() {
+    Node *node = mul();
+
+    for (;;) {
+        if (consume('+')) {
+            node = new_node(ND_ADD, node, mul());
+        } else if (consume('-')) {
+            node = new_node(ND_SUB, node, mul());
+        } else {
+            return node;
+        }
+    }
+}
+
+Node *mul() {
+    Node *node = primary();
+
+    for (;;) {
+        if (consume('*')) {
+            node = new_node(ND_MUL, node, primary());
+        } else if (consume('/')) {
+            node = new_node(ND_DIV, node, primary());
+        } else {
+            return node;
+        }
+    }
+}
+
+Node *primary() {
+    if (consume('(')) {
+        Node *node = expr();
+        expect(')');
+        return node;
+    }
+
+    return new_node_num(expect_number());
+}
 
 int main(int argc, char **argv) {
     if (argc != 2) {
